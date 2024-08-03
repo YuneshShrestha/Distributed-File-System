efcd23ab0b8231b5aa979809fda972b33b75c31a
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2024-08-03 19:36:51 +0545

    feat: Implement CASPathTransformFunc for content addressable storage and W/R of file

--- store.go
+++ store.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+// CAS = Content Addressable Storage is a method of storing data such that it is retrievable based on its content, not its location.
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i+1)*blockSize
+		paths[i] = hashStr[from:to]
+	}
+	return PathKey{
+		Pathname: strings.Join(paths, "/"),
+		Filename: hashStr,
+	}
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	Pathname string
+	Filename string
+}
+
+func (p PathKey) FullPath() string {
+	return p.Pathname + "/" + p.Filename
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
@@ -26,14 +56,13 @@ func NewStore(opts StoreOpts) *Store {
 	}
 }
 
-func (s *Store) writeStream(path string, r io.Reader) error {
-	path = s.PathTransformFunc(path)
-	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathKey := s.PathTransformFunc(key)
+	if err := os.MkdirAll(pathKey.Pathname, os.ModePerm); err != nil {
 		return err
 	}
-	filename := "somefile"
-	pathAndFilename := path + "/" + filename
 
+	pathAndFilename := pathKey.FullPath()
 	f, err := os.Create(pathAndFilename)
 	if err != nil {
 		return err
@@ -44,4 +73,24 @@ func (s *Store) writeStream(path string, r io.Reader) error {
 	}
 	log.Printf("wrote %d bytes to %s", n, pathAndFilename)
 	return nil
+}
+
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return buf, nil
+
+}
+
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
 }
